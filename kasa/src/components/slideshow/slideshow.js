8b0c3db5c6dc87fa5c22a9d4f1c6bc8188cca3a0
Jerry Brézout <jerry/brzt@gmail.com>
2023-02-11 18:46:03 +0100

    réalisation de la lightbox

--- kasa/src/components/slideshow/slideshow.js
+++ kasa/src/components/slideshow/slideshow.js
@@ -1,13 +1,35 @@
 import './slideshow.css'
 import logoArrowLeft from '../../assets/logo/logo_arrow_left.svg'
 import logoArrowRight from '../../assets/logo/logo_arrow_right.svg'
+import { useState } from 'react'
 
-export default function Slideshow() {
-    return(
+export default function Slideshow({ location }) {
+
+    const [index, setIndex] = useState(0)
+
+
+    function slides(e) {
+        if (e.target.classList.contains('slideshow-arrowLeft')) {
+
+            index === 0 ? setIndex(location.pictures.length - 1) : setIndex(index - 1);
+
+        }
+
+        if (e.target.classList.contains('slideshow-arrowRight')) {
+
+            index === location.pictures.length - 1 ? setIndex(0) : setIndex(index + 1)
+
+        }
+
+    }
+
+
+    return (
         <section className='slideshow'>
-            <img className='slideshow-img' src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg" alt="location" />
-            <img className='slideshow-arrowLeft' src={logoArrowLeft} alt='' />
-            <img className='slideshow-arrowRight' src={logoArrowRight} alt='' />
+            <img className='slideshow-img' src={location.pictures[index]} alt="location" />
+            <img onClick={(e) => slides(e)} className='slideshow-arrowLeft' src={logoArrowLeft} alt='' />
+            <img onClick={(e) => slides(e)} className='slideshow-arrowRight' src={logoArrowRight} alt='' />
+            <p className='slideshow-page'>{index + 1} / {location.pictures.length} </p>
         </section>
     )
 }
--- kasa/src/pages/housing/housing.js
+++ kasa/src/pages/housing/housing.js
@@ -1,7 +1,11 @@
 import React from 'react'
-// import datas from '../../data/data.json'
+import datas from '../../data/data.json'
 import { useParams } from 'react-router-dom'
+import Collapse from '../../components/collapse/collapse'
 import Slideshow from '../../components/slideshow/slideshow';
+import './housing.css'
+import logoStarGrey from '../../assets/logo/logo_star_grey.svg'
+import logoStarRed from '../../assets/logo/logo_star_red.svg'
 
 
 /**
@@ -12,8 +16,35 @@ import Slideshow from '../../components/slideshow/slideshow';
 export default function Housing() {
   const {idHousing} = useParams();
   
+  const housing = idHousing && datas.find(housing => housing.id === idHousing)
+  const rating = [1,2,3,4,5]
 
+ 
   return (
-    <Slideshow />
+    <main className='housing'>
+    <Slideshow location = {housing}  />
+    <div className='housing-description'>
+        <h3>{housing.title}</h3>
+        <div className='housing-profil'>
+          <p className='housing-name'>{housing.host.name}</p>
+          <img className='housing-photo' src={housing.host.picture} alt="user"></img>
+        </div>
+    </div>
+    <p className='housing-localisation'>{housing.location}</p>
+    <div className='housing-informations'>
+      <div className='housing-tags'>
+        {housing.tags.map(map => <span className='housing-tag' key={map}>{map}</span>)}
+      </div>
+      <div className='housing-rating'>
+      {rating.map((star) => {
+        return star <=  housing.rating ? <img key={star} src={logoStarRed} alt="logo-star"></img> : <img key={star} src={logoStarGrey} alt="logo-star"></img>
+      })}
+      </div>
+    </div>
+      <div className='housing-collapse'>
+        <Collapse title="Description" description={housing.description} />
+        <Collapse title="Equipements" description={housing.equipments} />
+      </div>
+    </main>
   )
 }
